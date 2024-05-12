a15967434c5f97a6f7049b28ca1f6650db7aae95
capricorncd <capricorncd@qq.com>
2024-05-12 21:29:25 +0900

    Update

--- modules/gemini.py
+++ modules/gemini.py
@@ -1,17 +1,17 @@
 import os, io, base64
-import google.generativeai as genai
+import google.generativeai as gen_ai
 from PIL import Image
 
 from typing import List
 from modules.CustomIterator import CustomIterator
 
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+gen_ai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 MODEL_VISION = 'gemini-pro-vision'
 
 def get_model_list() -> List[str]:
     l = []
-    for m in genai.list_models():
+    for m in gen_ai.list_models():
         if 'generateContent' in m.supported_generation_methods:
             l.append(m.name.replace('models/', ''))
     return l
@@ -24,7 +24,7 @@ def chat_gemini(ci: CustomIterator, data: dict):
         input = data.get('input')
         model_name = data.get('model')
         base64_str = data.get('imgData')
-        model = genai.GenerativeModel(model_name)
+        model = gen_ai.GenerativeModel(model_name)
         response = None
         if model_name == MODEL_VISION and base64_str is not None:
             img = Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
